895de0b0d44fd10deb11baa1eec18ddc2a0022c5
WooHaem <whm1001@4-m.co.kr>
2022-08-02 17:37:55 +0900

    부트스트랩 추가 - 2022.08.02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+/* eslint-disable */
+
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import 메인이미지 from './img/main-bg.jpg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      gitg
+      <Navbar variant="dark" className='navbar'>
+        <Container>
+          <Navbar.Brand href="#home">Wshop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">Board</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{background: 'url(' + 메인이미지 + ')' }}></div>
+      
+      <Container>
+        <Row>
+          <Col sm>상품</Col>
+          <Col sm>상품</Col>
+          <Col sm>상품</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
