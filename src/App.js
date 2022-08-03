08aa7216f41d0a6e40842e3bc2fa0858c9cd1b11
WooHaem <whm5020@gmail.com>
2022-08-03 18:19:31 +0900

    2022.08.03 - 상품리스트 종합 기본기 활용 with import / export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 /* eslint-disable */
 
+import { useState } from 'react';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import 메인이미지 from './img/main-bg.jpg';
 
 import './App.css';
+import data from './data.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  let [prd] = useState(data);
+  
   return (
     <div className="App">
       <Navbar variant="dark" className='navbar'>
@@ -20,16 +25,34 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg' style={{background: 'url(' + 메인이미지 + ') center center no-repeat' }}></div>
+      <div className='main-bg' style={{background: 'url(' + 메인이미지 + ') center center no-repeat', backgroundSize: 'cover' }}>
+        
+      </div>
       
       <Container>
         <Row>
-          <Col sm>상품</Col>
-          <Col sm>상품2</Col>
-          <Col sm>상품3</Col>
+          <PrdList 데이터= {prd}/>
         </Row>
       </Container>
     </div>
+  );
+}
+
+function PrdList(props) {
+
+  return (
+
+    props.데이터.map(function(a, i) {
+      return (
+        <Col sm key={i}>
+          {/* <img src={process.env.PUBLIC_URL + '/img' + (i + 1) + '.jpg'} width="100%" /> public 경로를 이용한 이미지 불러오기 */}
+          <img src={props.데이터[i].img} width="100%" /> {/* 데이터에 이미지 추가 */}
+          <h4>{props.데이터[i].title}</h4>
+          <p>{props.데이터[i].content}</p>
+        </Col>
+      )
+    })
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000,
+      img : "http://hoopbro.co.kr/web/product/big/202203/7248447b83ef78150dda5b91f9509da0.jpg"
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000,
+      img : "https://image.koibito.co.kr/product_photo/202104/1618375867_b_1_IMG_4108.JPG"
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000,
+      img : "https://image.msscdn.net/images/goods_img/20210810/2056622/2056622_1_500.jpg"
+    }
+] 
+
+export default data;
